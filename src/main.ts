aaf7a3c3e1a3bef4fa05e8b521e45d9dad60b294
SteveBunlon <bunlon.steve@gmail.com>
2019-12-02 17:24:29 +0100

    Edit origins

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin: '*'});
+  app.enableCors({origin: ['http://app.development.forestadmin.com', 'https://app.development.forestadmin.com']});
   app.use(cookieParser());
   await app.listen(80);
 }
