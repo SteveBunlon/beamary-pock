9d558224427f211bf8975887c934885b0f329268
SteveBunlon <bunlon.steve@gmail.com>
2019-12-02 15:36:37 +0100

    add post request

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -1,10 +1,22 @@
-import { Controller, Get, HttpCode, HttpStatus, Res } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Post, Res } from '@nestjs/common';
 
 @Controller('login')
 export class LoginController {
   @Get()
   @HttpCode(HttpStatus.OK)
   login(@Res() res): string {
+    const sessionValue = `session-${Math.random()}`;
+    res.cookie('beamary-session-cookie', sessionValue, {
+      httpOnly: true,
+    });
+
+    res.send(`Successfully logged in ! Your session is : ${sessionValue}`);
+    return null;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.OK)
+  postLogin(@Res() res): string {
     const sessionValue = `session-${Math.random()}`;
     res.cookie('beamary-session-cookie', sessionValue, {
       httpOnly: true,
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin: ['app.development.forestadmin.com']});
+  app.enableCors({origin: '*'});
   app.use(cookieParser());
   await app.listen(80);
 }
