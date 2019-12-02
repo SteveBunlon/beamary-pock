da22726d15a8a123e9f4b613fc567c8d207553d4
SteveBunlon <bunlon.steve@gmail.com>
2019-12-02 15:23:29 +0100

    Add cors origin

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({origin: ['app.development.forestadmin.com']});
   app.use(cookieParser());
   await app.listen(80);
 }
